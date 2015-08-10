922ec58ba01a7acc1c9482b3280bf36437d9621d
Andrew Graham <andrew.t.graham@live.com>
2015-08-10 17:36:29 +0000

    Require the -right- user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,16 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
-  # Remember a suser in a persistent session
+  # Remember a user in a persistent session
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
+  end
+  
+  # Returns true if the given user is the current user
+  def current_user?(user)
+    user == current_user
   end
   
   # Return the current logged-in user (if any - OR EQUALS)
