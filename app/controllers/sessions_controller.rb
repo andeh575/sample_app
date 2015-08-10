a30c2ee1cf7d1f9bbf0d2b73a645e48d91ede54f
Andrew Graham <andrew.t.graham@live.com>
2015-08-10 14:49:26 +0000

    Implement outline of sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    # Find the user in the DB by email address
+    user = User.find_by(email: params[:session][:email].downcase)
+    
+    # Returns true only if the user is found and the password is valid
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page
+    else
+      # Create an error message
+      flash[:danger] = "Invalid email/password combination" 
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+  
 end
