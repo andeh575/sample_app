b51f2033125a513013148ce2cab5b15a410c7a7e
Andrew Graham <andrew.t.graham@live.com>
2015-08-11 15:27:50 +0000

    Implement activation to new DB seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessor   :remember_token, :activation_token
   before_save     :downcase_email
-  before_create:  :create_activation_digest
+  before_create   :create_activation_digest
   
   
   validates :name, presence: true, length: {maximum: 50}
